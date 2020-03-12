52ab69dd2e10c751acfbbbb776873e30d1d2f673
Vrajesh Rana <vur2@njit.edu>
2020-03-12 19:03:59 -0400

    added local data storage, tests are a WIP

--- backend/app.js
+++ backend/app.js
@@ -2,25 +2,67 @@ var express = require("express");
 var socket = require("socket.io");
 var app = express();
 
-server = app.listen(8080, function() {
-  console.log("Server is running on port 8080");
+server = app.listen(8080, () => {
+  console.log("server is running on port 8080");
 });
 
 const io = socket(server);
+const fs = require("fs");
 
 let currentlyActiveUsers = [];
 let messageList = [];
 let messageNumber = 1;
 
+let updateLocalStorage = () => {
+  fs.writeFileSync(
+    "./log.json",
+    JSON.stringify({
+      messages: messageList,
+      savedID: messageNumber
+    }),
+    err => {
+      if (err) {
+        console.error(err);
+        return;
+      } else {
+        console.log("Local file has been created");
+      }
+    }
+  );
+};
+
+let retrieveLocalStorage = async () => {
+  if (fs.existsSync("./log.json")) {
+    storageInfo = await JSON.parse(fs.readFileSync("./log.json"));
+    messageList = storageInfo.messages;
+    messageNumber = storageInfo.savedID;
+    return;
+  } else {
+    fs.writeFileSync("./log.json", [], err => {
+      if (err) {
+        console.err(err)
+        return;
+      }
+    });
+  }
+};
+
+retrieveLocalStorage();
+
 io.on("connection", socket => {
   console.log("PING TO SERVER");
 
+  socket.on('PING', () => {
+    socket.emit('PONG', "Hello world!")
+  })
+
   socket.on("USER_CONNECTED", data => {
     /*
-      When use connects, add the unique socket ID to active users list
+      When user connects, check local storage for any data and set message list
+      Then, add the unique socket ID to active users list
       Finally, emit the current active list & unique socket identifier       
     */
-   console.log("USER HAS CONNECTED WITH NAME & ID : ", data, socket.id);
+    console.log("USER HAS CONNECTED WITH NAME : ", data, socket.id);
     currentlyActiveUsers.push({
       user: data.connectedUser,
       id: socket.id
@@ -59,5 +101,6 @@ io.on("connection", socket => {
     
     io.emit("CHATROOM_UPDATE", currentlyActiveUsers);
     io.emit("MESSAGE_LIST_UPDATE", messageList);
+    updateLocalStorage();
   });
 });
